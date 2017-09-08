0814f2cf78e7320e925d71cd4f7f8ebb0d0e3fd9
sourav-dev-meme <sourav.das@memeinfotech.com>
2017-09-08 12:18:34 +0530

    first commit 2

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Device } from '@ionic-native/device';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController , public device: Device) 
+  {
+    alert('Device UUID is: ' + this.device.uuid);
+  }
+
+}
